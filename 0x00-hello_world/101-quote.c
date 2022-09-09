188a93647e16fd2a3d01f6c3459a7021b78f47e7
Victor Ngozi <ngozieze.jr@gmail.com>
2022-09-08 20:52:52 -0700

    Advanced task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
-#include <string.h>
 #include <unistd.h>
 
 /**
- * main - main block
- * Return: 1
+ * main - Entry point
+ * Return:1
  */
 
 int main(void)
 {
-	int s = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
-write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", s);
+char ch[] = "and that piece of art is useful\" - Dora Korpa, 2015-10-19\n";
+write(2, ch, sizeof(ch));
 return (1);
 }
-
