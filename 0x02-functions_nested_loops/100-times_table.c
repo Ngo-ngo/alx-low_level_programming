31634c5935c7ce5d91cc512d014c425183a7986d
Victor Ngozi <ngozieze.jr@gmail.com>
2022-09-13 18:06:37 -0700

    Task 0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,28 +11,28 @@ void print_times_table(int n)
 
 int a = 0, rep, b;
 
-if (n < 0 || n > 15)
+	if (n < 0 || n > 15)
 
-	return;
+		return;
 
-while (a <= n)
-{
-
-	for (b = 0; b <= n; b++)
+	while (a <= n)
 	{
 
-		rep = a * b;
-
-		if (b == 0)
-
-			_putchar('0' + rep);
-
-		else if (rep < 10)
+		for (b = 0; b <= n; b++)
 		{
 
-			_putchar(' ');
-			_putchar(' ');
-			_putchar('0' + rep);
+			rep = a * b;
+
+			if (b == 0)
+
+				_putchar('0' + rep);
+
+			else if (rep < 10)
+			{
+
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + rep);
 
 		}
 
