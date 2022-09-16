825cafe1512b520d35c10c8f2701fea371c4ef2c
Victor Ngozi <ngozieze.jr@gmail.com>
2022-09-15 18:39:41 -0700

    Task 11:Advanced

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,24 +8,22 @@
 
 int main(void)
 {
+unsigned int long n = 612852475143, a = (int) sqrt(n);
 
-	unsigned int long n = 612852475143, a = (int) sqrt(n);
+while (1)
+{
 
-	while (1)
+	if (n % a == 0)
 	{
+		printf("%lu \n", n / a);
 
-		if (n % a == 0)
-		{
-			printf("%lu \n", n / a);
-
-			break;
-
-		}
-
-		a--;
-
+		break;
 	}
 
+	a--;
 
-	return (0);
+}
+
+return (0);
+
 }
