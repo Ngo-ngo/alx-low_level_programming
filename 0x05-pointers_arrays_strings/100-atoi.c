4543cdf79af523e96fae035efb5e12ca68f4d6c3
Victor Ngozi <ngozieze.jr@gmail.com>
2022-09-19 20:14:18 -0700

    Task 10: Advanced

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -21,7 +21,6 @@ int _atoi(char *s)
 	{
 
 		if (s[i] == '-')
-
 			p *= -1;
 
 
@@ -29,17 +28,13 @@ int _atoi(char *s)
 		{
 
 			if (h < 0)
-
 				h = (h * 10) - (s[i] - '0');
 
 			else
-
 				h = (s[i] - '0') * -1;
 
 
-
 			if (s[i + 1] < 48 || s[i + 1] > 57)
-
 				break;
 
 		}
@@ -47,9 +42,7 @@ int _atoi(char *s)
 	}
 
 	if (p < 0)
-
 		h *= -1;
-
 
 	return (h);
 
