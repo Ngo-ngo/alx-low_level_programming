e3608e0b98f9b2c5afe2e895a7d6d6af60561274
Victor Ngozi <ngozieze.jr@gmail.com>
2022-09-19 20:11:09 -0700

    Task 10: Advanced

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,37 +1,56 @@
-#include"main.h"
+#include "main.h"
 
 /**
- * _atoi - convert a string to an integer.
- * @s: char type string
- * Return: integer converted
+ * _atoi - Convert a string to integer.
+ * @s: char array string
+ * Return: first integer found in string
  */
 
 int _atoi(char *s)
 {
 
-	int sign = 1, resp = 0, firstNum;
+	int i;
 
-	for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
+	int h, p;
+
+	h = 0;
+
+	p = -1;
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
 
-		if (s[firstNum] == '-')
+		if (s[i] == '-')
+
+			p *= -1;
+
+
+		if (s[i] > 47 && s[i] < 58)
 		{
 
-			sign *= -1;
+			if (h < 0)
+
+				h = (h * 10) - (s[i] - '0');
+
+			else
+
+				h = (s[i] - '0') * -1;
+
+
+
+			if (s[i + 1] < 48 || s[i + 1] > 57)
+
+				break;
 
 		}
 
 	}
 
-	for (int i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
-	{
+	if (p < 0)
 
-		resp *= 10;
+		h *= -1;
 
-		resp += (s[i] - 48);
 
-	}
-
-	return (sign * resp);
+	return (h);
 
 }
