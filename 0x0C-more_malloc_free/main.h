967d493d503b5fc20fb5bcc40cb4476fbf702ebd
Victor Ngozi <ngozieze.jr@gmail.com>
2022-10-06 00:00:13 -0700

    Header file

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,13 +1,11 @@
-#if !defined(MAIN_H)
+#ifndef MAIN_H
 #define MAIN_H
-#include <stdlib.h>
 #include <stdio.h>
-char *create_array(unsigned int size, char c);
-char *_strdup(char *str);
-char *str_concat(char *s1, char *s2);
-int **alloc_grid(int width, int height);
-void free_grid(int **grid, int height);
-char *argstostr(int ac, char **av);
-char **strtow(char *str);
 
-#endif
+int _putchar(char);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+
+#endif /*MAIN_H */
