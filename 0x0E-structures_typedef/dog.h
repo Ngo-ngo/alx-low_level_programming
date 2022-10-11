70b9d6164919eda7ad5c06b5df55cd966cef73f2
Victor Ngozi <ngozieze.jr@gmail.com>
2022-10-10 17:04:00 -0700

    Task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,14 +2,13 @@
 #define DOG_H
 
 /**
- * struct dog - a data type with poppy structure
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
- * Description: represente a dog, his name, age and his owner
+ * struct dog - structure definition of a dog
+ * @name: character string
+ * @age: integer
+ * @owner: character string
  */
 
-struct dog
+typedef struct dog
 {
 
 	char *name;
@@ -18,28 +17,13 @@ struct dog
 
 	char *owner;
 
-};
-
-
-
-/**
-
- * dog_t - typedef for struct dog
-
- */
-
-typedef struct dog dog_t;
-
-
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
 void print_dog(struct dog *d);
-
 dog_t *new_dog(char *name, float age, char *owner);
-
 void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 
-
-
-#endif /* DOG_H */
+#endif
