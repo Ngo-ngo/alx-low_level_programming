c75246a04be79d8321f30db009d44efefd09a764
Victor Ngozi <ngozieze.jr@gmail.com>
2022-11-01 16:03:06 -0700

    Task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,15 +1,27 @@
-#include "main.h"
+#include "holberton.h"
+
+
+
 #define MAXSIZE 1204
+
 #define SE STDERR_FILENO
 
+
+
 /**
+
  * main - create the copy bash script
+
  * @ac: argument count
+
  * @av: arguments as strings
+
  * Return: 0
+
  */
 
 int main(int ac, char *av[])
+
 {
 
 	int input_fd, output_fd, istatus, ostatus;
@@ -17,6 +29,8 @@ int main(int ac, char *av[])
 	char buf[MAXSIZE];
 
 	mode_t mode;
+
+
 
 	mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
@@ -27,8 +41,8 @@ int main(int ac, char *av[])
 	input_fd = open(av[1], O_RDONLY);
 
 	if (input_fd == -1)
-	    
-	      dprintf(SE, "Error: Can't read from file %s\n", av[1]), exit(98);
+
+		dprintf(SE, "Error: Can't read from file %s\n", av[1]), exit(98);
 
 	output_fd = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, mode);
 
@@ -41,6 +55,7 @@ int main(int ac, char *av[])
 		istatus = read(input_fd, buf, MAXSIZE);
 
 		if (istatus == -1)
+
 		{
 
 			dprintf(SE, "Error: Can't read from file %s\n", av[1]);
@@ -50,14 +65,16 @@ int main(int ac, char *av[])
 		}
 
 		if (istatus > 0)
+
 		{
 
 			ostatus = write(output_fd, buf, (ssize_t) istatus);
 
 			if (ostatus == -1)
+
 			{
 
-			     dprintf(SE, "Error: Can't write to %s\n", av[2]);
+				dprintf(SE, "Error: Can't write to %s\n", av[2]);
 
 				exit(99);
 
